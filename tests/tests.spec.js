2676f39048a64231b20da3b3e6dd56a77569ea77
имя фамилия < >
2024-11-12 17:44:20 +0300

    some tests added

--- tests/tests.spec.js
+++ tests/tests.spec.js
@@ -3,10 +3,17 @@ import { test, expect } from '@playwright/test';
 
 test('step1', async ({ page }) => {
   await page.goto('http://localhost:8080');
+  const navigation = await page.getByRole('navigation');
+  await expect(navigation).toBeVisible()
+
+
 });
 
 test('step2', async ({ page }) => {
   await page.goto('http://localhost:8080');
+  const link = await page.getByRole('link').first()
+  await link.hover();
+  await expect(link).toHaveCSS('color', 'rgb(189, 152, 121)')
 });
 
 test('step3', async ({ page }) => {
